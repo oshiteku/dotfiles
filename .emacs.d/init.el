3d5a6ee7614d89fe7b2ac6bbf7440d0e1fa9496a
oshiteku <oshiteku@gmail.com>
2024-12-08 01:46:13 +0900

    🔧 spacious-padding

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -133,6 +133,20 @@
 ;;
 ;; Style, UI
 
+(leaf spacious-padding
+  :ensure t
+  :global-minor-mode t
+  :custom
+  ((spacious-padding-widths
+    '( :internal-border-width 15
+       :header-line-width 4
+       :mode-line-width 6
+       :tab-width 4
+       :right-divider-width 30
+       :scroll-bar-width 8
+       :fringe-width 8))
+   (spacious-padding-subtle-mode-line . `( :mode-line-active 'default
+                                           :mode-line-inactive vertical-border))))
 (leaf font
   :config
   (set-language-environment "Japanese")
